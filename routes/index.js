aa51f034cf7314c0d33b207e0971e761cdf575a6
Sudhakar <sramakrishnan@admins-MacBook-Pro.local>
2019-09-08 18:01:07 +0530

    First version of medai xray upload

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function(req, res, next) {
+  res.render("index", {
+    title: "Welcome to MedAI - Fracture Prediction Made Simple"
+  });
 });
 
 module.exports = router;
